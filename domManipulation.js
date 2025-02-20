543ebb4f653772eca64e160c974e7ff20c098b54
shaay98 <nrichardson567@icloud.com>
2025-02-20 03:55:32 +0000

    Dom-Manipulation using JS practice

---
+++ domManipulation.js
@@ -0,0 +1,18 @@
+`use strict`;
+
+const p = document.getElementById ("mp-one");
+console.log(p);
+
+const footer = document.querySelector (".footer");
+console.log(footer);
+
+let button = document.getElementById ("btn");
+button.addEventListener (click,) function (click) {
+    alert ("thank you for submitting"); 
+}
+
+const a = document.querySelector ("link");
+a.addEventListener (click, function (event) {
+    event.preventDefault ();
+    console.log(Link);
+};
